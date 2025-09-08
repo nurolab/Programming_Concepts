ffc187992dc9d0094cd8334e09cbc5a432cea573
depthgr8 <depthgr8@gmail.com>
2025-09-08 14:53:17 +0530

    Testing C environment with GCC an VS Code

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>              //  Include information about standard library
+int main()                          //  define a function called main that received no argument values
+{                               //  statements of main are enclosed in braces
+    printf("hello, world\n");      //  main calls library function printf
+    return 0;                            //  to print this sequence of characters
+} 
+
+/*
+Exercise 1−1. Run the ``hello, world'' program on your system. Experiment with leaving out parts of the
+program, to see what error messages you get.
+
+Exercise 1−2. Experiment to find out what happens when prints's argument string contains \c, where c is
+some character not listed above.
+
+*/
