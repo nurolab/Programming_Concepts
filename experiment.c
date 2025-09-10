d48ad346992e5f55db246a58105e556b2d4702aa
Sonal Gupta <sonalg974@gmail.com>
2025-09-10 11:41:52 +0530

    Time travel.. commiteed on 24/11/25

---
+++ experiment.c
@@ -0,0 +1,6 @@
+#include <stdio.h>              //  Include information about standard library
+int main()                          //  define a function called main that received no argument values
+{                               //  statements of main are enclosed in braces
+    printf("Time Travel back.\n"); // Edited by Sonal       //  main calls library function printf
+    return 0;                            //  to print this sequence of characters
+} 
