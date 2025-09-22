3f6b749cd457681a5451b40e0ad3326f7f72bf6f
SONAL GUPTA <sonalg974@gmail.com>
2025-09-22 15:52:31 +0530

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>              //  Include information about standard library
 int main()                          //  define a function called main that received no argument values
 {                               //  statements of main are enclosed in braces
-    printf("hello, world\n");      //  main calls library function printf
+    printf("Hello world !!!!\n"); // Edited by Sonal       //  main calls library function printf
     return 0;                            //  to print this sequence of characters
 } 
 
