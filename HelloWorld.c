1195f539e0c2ea0e04a3dd54adbfcaa70f3a2c2b
Manasvi Sharma <sharmateena565@gmail.com>
2025-10-04 12:23:02 +0530

    Update HelloWorld.c
    removed unwanted line

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,6 @@
 #include<stdio.h>              //  Include information about standard library
 int main()                          //  define a function called main that received no argument values
-{                               //  statements of main are enclosed in braces
-<<<<<<< HEAD
+{              
     printf("Hello world !!!!\n");    //  main calls library function printf
     return 0;                            //  to print this sequence of characters
 >>>>>>> ae4e4d738889a43413dab8548b21c5c3171194fe
